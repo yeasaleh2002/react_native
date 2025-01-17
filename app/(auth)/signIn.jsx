fad8cd73c202eae2ff1fea57bcb5af6d9ee6545d
Yeasaleh <yeasalehofficial2002@gmail.com>
2025-01-17 15:38:18 +0600

    form field

--- app/(auth)/signIn.jsx
+++ app/(auth)/signIn.jsx
@@ -1,18 +1,56 @@
 import { Image, ScrollView, Text, View } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import logo from "../../assets/images/logo.png";
+import FormField from "../../components/FormField";
 
 const SignIn = () => {
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+
   return (
     <SafeAreaView className="bg-primary h-full">
       <ScrollView className="">
         <View className="w-full justify-center h-full px-4 my-6">
-          <Image
+          {/* <Image
             source={logo}
             className="w-[115px] h-[30px]"
             // style={{height:"30px", width:"115px"}}
             resizeMethod="contain"
+          /> */}
+          <Text
+            style={{ color: "#FF9C01" }}
+            className="text-white text-4xl text-semibold font-psemibold"
+          >
+            Aora
+          </Text>
+          <Text className="text-2xl text-white text-semibold mt-10 font-psemibold">
+            Login in to Aora
+          </Text>
+          <FormField
+            title="Email"
+            value={form.email}
+            handleChangeText={() =>
+              setForm({
+                ...form,
+                email: e,
+              })
+            }
+            otherStyles="mt-7"
+            keyboardType="email-address"
+          />
+          <FormField
+            title="Email"
+            value={form.password}
+            handleChangeText={() =>
+              setForm({
+                ...form,
+                password: e,
+              })
+            }
+            otherStyles="mt-7"
           />
         </View>
       </ScrollView>
---
+++ components/FormField.jsx
@@ -0,0 +1,14 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const FormField = () => {
+  return (
+    <View className="space-y-2">
+      <Text style={{ color: "#FFF" }} className="">
+        Form Field
+      </Text>
+    </View>
+  );
+};
+
+export default FormField;
